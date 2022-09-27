81b568d3b87886efe6acd87001300c3f37bc32b0
JustOxynite <john.sixxteen@gmail.com>
2022-09-27 22:03:49 +0300

    .

---
+++ code/laba1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define e exp(1)
+
+int main(){
+	string in;
+	double x;
+	cin >> in;
+	if (in == "null") //сами инициализиреум x
+	else x = stod(in);
+	cout << x;
+	
+
+	return 0;
+}
